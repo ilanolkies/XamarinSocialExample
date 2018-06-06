b9f2f51f5db4793a3c1d28238c2c251c78d13bfc
Ilan Olkies <ilanolkies@outlook.com>
2018-06-06 19:11:17 -0300

    Add ISocial Android implementation.

---
+++ XamarinSocial.Android/SocialService.cs
@@ -0,0 +1,20 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using XamarinSocial.Droid;
+
+[assembly: Dependency(typeof(SocialService))]
+namespace XamarinSocial.Droid
+{
+	public class SocialService : ISocial
+    {      
+		public void Share(string content)
+		{         
+			Intent share = new Intent(Intent.ActionSend)
+				.SetType("text/plain")
+				.AddFlags(ActivityFlags.ClearWhenTaskReset)
+				.PutExtra(Intent.ExtraText, content);
+
+			Android.App.Application.Context.StartActivity(Intent.CreateChooser(share, "Compartir!"));
+		}
+	}
+}
